685bb7d91850d48ed2a1f8fe78e0151c63a6fb18
dawood8080 <dawood_khader@hotmail.com>
2025-10-13 00:39:00 +0300

    Database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const mockUrls = [
  'https://atybhqqgzk.ufs.sh/f/t2uY3skTYXDOoMh1k8OjynkiSNzmlPfHeCEthRUAVux3g0c4',
  'https://atybhqqgzk.ufs.sh/f/t2uY3skTYXDOEipupbcT49Pqwm6rlSG3HvnzI2pMRNkoaJFj'
@@ -10,11 +12,19 @@ const mockImages = mockUrls.map((url, index) => {
   };
 });
 
-export default function HomePage() {
+export default async function HomePage() {
+  
+  const posts = await db.query.posts.findMany();
+  console.log('🚀 ~ HomePage ~ posts:', posts);
+
   return (
     <main className="">
-        <div className="flex flex-wrap gap-1">{mockImages.map((image) => (
-          <div key={image.id} className="w-48 ">
+        <div className="flex flex-wrap gap-1">
+          {posts.map((post, index) => (
+            <div key={post.id + index} >{post.id}</div>
+          ))}
+          {mockImages.map((image, index) => (
+          <div key={image.id + index} className="w-48 ">
             <img src={image.url} />
           </div>
         ))}</div>
