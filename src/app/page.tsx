8426fa466df083f6df94d30fe742bd394742a7c3
dawood8080 <dawood_khader@hotmail.com>
2025-10-23 00:18:23 +0300

    use auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,38 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 // since there is caching, the next line make sure every time we change in the DB, the content is updated
 export const dynamic = "force-dynamic"
 
-
-export default async function HomePage() {
-
+async function Images () {
   const images = await db.query.images.findMany({
     orderBy: (images, { desc }) => [desc(images.createdAt)],
   });
 
   return (
-    <main className="">
-        <div className="flex flex-wrap gap-1">
-          {[...images, ...images, ...images].map((image, index) => (
-            <div className="flex flex-col gap-0.5" key={image.name + image.url + index}>
-              <img src={image.url}  className="w-48 h-48 object-cover" />
-              <div className="text-sm text-white">
-                {image.name}
-              </div>
-            </div>
-          ))}
+    <div className="flex flex-wrap gap-1">
+      {[...images, ...images, ...images].map((image, index) => (
+        <div className="flex flex-col gap-0.5" key={image.name + image.url + index}>
+          <img src={image.url}  className="w-48 h-48 object-cover" />
+          <div className="text-sm text-white">
+            {image.name}
+          </div>
         </div>
+      ))}
+    </div>
+  )
+}
+
+
+export default async function HomePage() {
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="h-full w-full text-2xl text-center">Please Sign in</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
