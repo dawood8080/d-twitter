ec833753b8d664c9aa00268c2244e9b7fe527abb
dawood8080 <dawood_khader@hotmail.com>
2025-11-01 00:11:28 +0300

    more cleanup on modal

--- src/app/@modal/(.)img/[id]/modal.tsx
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -19,9 +19,8 @@ export function Modal({ children }: { children: React.ReactNode }) {
   }
 
   return createPortal(
-    <dialog ref={dialogRef} className="w-screen h-screen bg-zinc-800/50 m-0" onClose={onDismiss}>
+    <dialog ref={dialogRef} className="w-screen h-screen bg-zinc-800/50 m-0 text-white" onClose={onDismiss}>
       {children}
-      <button onClick={onDismiss} className="close-button" />
     </dialog>,
     document.getElementById('modal-root')!
   );
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,7 +1,19 @@
 import { getImage } from "~/server/db/queries";
-import Image from "next/image";
 
 export default async function FullPageImageView({ id }: { id: number }) {
   const image = await getImage(id);
-  return <Image src={image.url} alt={image.name} width={400} height={400} />;
+  return (
+    <div className="h-full w-full flex items-center  justify-center bg-green-500/50 min-w-0 min-h-0">
+      <div className="flex flex-shrink items-center justify-center">
+        <img
+          src={image.url}
+          alt={image.name}
+          className="object-contain flex-shrink max-h-screen"
+        />
+      </div>
+      <div className="w-48 flex flex-col flex-shrink-0 border-l h-full">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
+  );
 }
