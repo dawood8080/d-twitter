5e4294e59456af1152e38ad6a42aefdff6bcc1ea
dawood8080 <dawood_khader@hotmail.com>
2025-11-02 23:30:32 +0300

    adding upload icon

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,20 +1,18 @@
-'use client'
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { UploadButton } from "../utils/uploadthing";
-import { useRouter } from "next/navigation";
+import { SimpleUploadButton } from "./simple-upload-button";
 
 export default function TopNav() {
-  const router = useRouter();
 
   return (
     <nav className="flex justify-between items-center w-full p-7 font-bold text-xl border-b-2 ">
       <div className="cursor-pointer">Gallery</div>
-      <div className="cursor-pointer flex gap-1">
+      <div className="cursor-pointer flex gap-4 items-center">
         <SignedOut>
           <SignInButton>Sign in</SignInButton>
         </SignedOut>
         <SignedIn>
-          <UploadButton endpoint="imageUploader" onClientUploadComplete={() => {router.refresh()}} />
+          {/* <UploadButton endpoint="imageUploader" onClientUploadComplete={() => {router.refresh()}} /> */}
+          <SimpleUploadButton />
           <UserButton />
         </SignedIn>
       </div>
