1b5b767a3daa324ff59649b9f49d5bb981d62c2e
dawood8080 <dawood_khader@hotmail.com>
2025-11-02 23:03:49 +0300

    Finalize Modal and Parallel route

--- src/app/@modal/(.)img/[id]/modal.tsx
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -19,7 +19,7 @@ export function Modal({ children }: { children: React.ReactNode }) {
   }
 
   return createPortal(
-    <dialog ref={dialogRef} className="w-screen h-screen bg-zinc-800/50 m-0 text-white" onClose={onDismiss}>
+    <dialog ref={dialogRef} className="w-screen h-screen bg-zinc-800/90 m-0 text-white" onClose={onDismiss}>
       {children}
     </dialog>,
     document.getElementById('modal-root')!
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,18 +1,29 @@
+import { currentUser } from "@clerk/nextjs/server";
 import { getImage } from "~/server/db/queries";
 
 export default async function FullPageImageView({ id }: { id: number }) {
   const image = await getImage(id);
+  const user = await currentUser();
+
   return (
-    <div className="h-full w-full flex items-center  justify-center bg-green-500/50 min-w-0 min-h-0">
-      <div className="flex flex-shrink items-center justify-center">
+    <div className="h-full w-full flex min-w-0 min-h-0">
+      <div className="flex flex-shrink items-center justify-center w-[70%]">
         <img
           src={image.url}
           alt={image.name}
-          className="object-contain flex-shrink max-h-screen"
+          className="object-contain flex-shrink max-h-full"
         />
       </div>
-      <div className="w-48 flex flex-col flex-shrink-0 border-l h-full">
-        <div className="text-xl font-bold">{image.name}</div>
+      <div className="w-48 flex flex-col flex-1 border-l h-full gap-2">
+        <div className="text-xl border-b text-center p-2">{image.name}</div>
+        <div className="flex flex-col p-2">
+          <span>Uploaded by:</span>
+          <span>{user?.fullName}</span>
+        </div>
+        <div className="flex flex-col p-2">
+          <span>Created at:</span>
+          <span>{new Date(image.createdAt).toLocaleString()}</span>
+        </div>
       </div>
     </div>
   );
