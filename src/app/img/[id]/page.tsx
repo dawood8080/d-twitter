8f6582c5a21a5dc7a0735271d685626596d31bcf
dawood8080 <dawood_khader@hotmail.com>
2025-11-14 16:15:10 +0300

    fix an error in the image page + delete cathc all

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,9 +1,15 @@
 import FullPageImageView from "~/components/full-image-page";
 
 export default async function PhotoPage({
-  params: { id: photoId },
+  params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
+  const photoId = (await params).id;
+
+  if (Number.isNaN(Number(photoId))) {
+    throw new Error("Invalid photo ID");
+  }
+
   return <FullPageImageView id={Number(photoId)} />;
 }
